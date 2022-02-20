4b91d0aa93ce3f509c30591d03d62ffbe9296628
Armanur Rahman <mubeenarman19@gmail.com>
2022-02-20 22:21:50 +0900

    add task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,27 +2,3 @@ const mongoose = require("mongoose");
 
 //connect to database
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
-
-// const Task = mongoose.model("Task", {
-//     description: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false,
-//     },
-// });
-
-// const task = new Task({
-//     description: "Dinner",
-// });
-
-// task.save()
-//     .then((result) => {
-//         console.log(result);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     });
